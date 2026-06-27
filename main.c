aabe26736aab09ed2a1c97de485ef1c4920e6b7c
Morita <morit958@icloud.com>
2026-06-27 14:59:58 +0900

    add binary operation

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include "chunk.h"
 #include "debug.h"
 
-#include "common.h"
 #include "vm.h"
 
 int main(int argc, const char *argv[]) {
@@ -22,11 +21,19 @@ int main(int argc, const char *argv[]) {
     writeChunk(&chunk, constant, 123);
     // 符号反転の命令を書き込む
     writeChunk(&chunk, OP_NAGATE, 123);
+
+    // 足し算をやる (定数命令と ADD 命令をチャンクに追加)
+    // -1.2 + 1.2
+    int num2 = addConstant(&chunk, 1.2);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, num2, 123);
+    writeChunk(&chunk, OP_ADD, 123);
+
     // リターン命令をチャンクに書き込む
     writeChunk(&chunk, OP_RETURN, 123);
 
     // チャンクの中身をデバッグ表示
-    // disassembleChunk(&chunk, "test chunk");
+    disassembleChunk(&chunk, "test chunk");
 
     /*
      * 3. VM でチャンクを実行
