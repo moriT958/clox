9cc2f16ad05a2ba90f72729203a1db95023da185
Morita <morit958@icloud.com>
2026-08-30 19:45:00 +0900

    hashTable: Define entry & table

---
+++ table.c
@@ -0,0 +1,15 @@
+#include <string.h>
+
+#include "memory.h"
+#include "table.h"
+
+void initTable(Table *table) {
+    table->count = 0;
+    table->capacity = 0;
+    table->entries = NULL;
+}
+
+void freeTable(Table *table) {
+    FREE_ARRAY(Entry, table->entries, table->capacity);
+    initTable(table);
+}
