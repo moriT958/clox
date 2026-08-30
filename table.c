63d9af8014ff0a22c020f6eefc37dd22151e962f
Morita <morit958@icloud.com>
2026-08-30 20:20:32 +0900

    hashTable: Search entry

--- table.c
+++ table.c
@@ -13,3 +13,26 @@ void freeTable(Table *table) {
     FREE_ARRAY(Entry, table->entries, table->capacity);
     initTable(table);
 }
+
+static Entry *findEntry(Entry *entries, int capacity, ObjString *key) {
+    uint32_t index = key->hash % capacity;
+    for (;;) {
+	Entry *entry = &entries[index];
+	if (entry->key == key || entry->key == NULL) {
+	    return entry;
+	}
+	index = (index + 1) % capacity;
+    }
+}
+
+bool tableGet(Table *table, ObjString *key, Value *value) {
+    if (table->count == 0)
+	return false;
+
+    Entry *entry = findEntry(table->entries, table->capacity, key);
+    if (entry->key == NULL)
+	return false;
+
+    *value = entry->value;
+    return true;
+}
--- table.h
+++ table.h
@@ -17,5 +17,6 @@ typedef struct {
 
 void initTable(Table *table);
 void freeTable(Table *table);
+bool tableGet(Table *table, ObjString *key, Value *value);
 
 #endif
