916f4e8a6af6ebbaa744c3271d70af959ae0a0de
Morita <morit958@icloud.com>
2026-09-11 23:23:10 +0900

    while 文を追加

--- compiler.c
+++ compiler.c
@@ -145,6 +145,17 @@ static void patchJump(int offset) {
     currentChunk()->code[offset + 1] = jump & 0xff;
 }
 
+static void emitLoop(int loopStart) {
+    emitByte(OP_LOOP);
+
+    int offset = currentChunk()->count - loopStart + 2;
+    if (offset > UINT16_MAX)
+	error("Loop body too large.");
+
+    emitByte((offset >> 8) & 0xff);
+    emitByte(offset & 0xff);
+}
+
 static uint8_t makeConstant(Value value) {
     int constant = addConstant(currentChunk(), value);
     if (constant > UINT8_MAX) {
@@ -559,6 +570,22 @@ static void ifStatement() {
     patchJump(elseJump);
 }
 
+static void whileStatement() {
+    int loopStart = currentChunk()->count;
+
+    consume(TOKEN_LEFT_PAREN, "Expect '(' after 'while'.");
+    expression();
+    consume(TOKEN_RIGHT_PAREN, "Expect ')' after condition.");
+
+    int exitJump = emitJump(OP_JUMP_IF_FALSE);
+    emitByte(OP_POP);
+    statement();
+    emitLoop(loopStart);
+
+    patchJump(exitJump);
+    emitByte(OP_POP);
+}
+
 static void expressionStatement() {
     expression();
     consume(TOKEN_SEMICOLON, "Expect ';' after expression.");
@@ -570,6 +597,8 @@ static void statement() {
 	printStatement();
     } else if (match(TOKEN_IF)) {
 	ifStatement();
+	} else if (match(TOKEN_WHILE)) {
+	whileStatement();
     } else if (match(TOKEN_LEFT_BRACE)) {
 	beginScope();
 	block();
