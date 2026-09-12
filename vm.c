a742ececf72da01d903cbc2968502fdd0b264148
Morita <morit958@icloud.com>
2026-09-12 18:00:50 +0900

    VM に CallFrame を追加する

--- vm.c
+++ vm.c
@@ -15,7 +15,10 @@
 
 VM vm;
 
-static void resetStack() { vm.stackTop = vm.stack; }
+static void resetStack() {
+    vm.stackTop = vm.stack;
+    vm.frameCount = 0;
+}
 
 static void runtimeError(const char *format, ...) {
     va_list args;
@@ -24,8 +27,9 @@ static void runtimeError(const char *format, ...) {
     va_end(args);
     fputs("\n", stderr);
 
-    size_t instruction = vm.ip - vm.chunk->code - 1;
-    int line = vm.chunk->lines[instruction];
+    CallFrame *frame = &vm.frames[vm.frameCount - 1];
+    size_t instruction = frame->ip - frame->function->chunk.code - 1;
+    int line = frame->function->chunk.lines[instruction];
     fprintf(stderr, "[line %d] in script\n", line);
 
     resetStack();
@@ -75,8 +79,11 @@ static void concatenate() {
 }
 
 static InterpretResult run() {
-#define READ_BYTE() (*vm.ip++)
-#define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+    CallFrame *frame = &vm.frames[vm.frameCount - 1];
+
+#define READ_BYTE() (*frame->ip++)
+#define READ_CONSTANT()                                                       \
+    (frame->function->chunk.constants.values[READ_BYTE()])
 #define READ_STRING() AS_STRING(READ_CONSTANT())
 
 // LEARN:
@@ -91,7 +98,7 @@ static InterpretResult run() {
 //
 // READ_SHORT は ip を 2 つ進めて、進めた分のバイトコードを返す
 #define READ_SHORT()                                                          \
-    (vm.ip += 2, (uint16_t)((vm.ip[-2] << 8) | vm.ip[-1]))
+    (frame->ip += 2, (uint16_t)((frame->ip[-2] << 8) | frame->ip[-1]))
 
 #define BINARY_OP(valueType, op)                                               \
     do {                                                                       \
@@ -113,7 +120,9 @@ static InterpretResult run() {
 	    printf(" ]");
 	}
 	printf("\n");
-	disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
+	disassembleInstruction(
+	    &frame->function->chunk,
+	    (int)(frame->ip - frame->function->chunk.code));
 #endif
 	uint8_t instruction;
 	switch (instruction = READ_BYTE()) {
@@ -211,33 +220,33 @@ static InterpretResult run() {
 
 	case OP_GET_LOCAL: {
 	    uint8_t slot = READ_BYTE();
-	    push(vm.stack[slot]);
+	    push(frame->slots[slot]);
 	    break;
 	}
 
 	case OP_SET_LOCAL: {
 	    uint8_t slot = READ_BYTE();
 	    // 代入式の値は式の結果として残すため pop しない。
-	    vm.stack[slot] = peek(0);
+	    frame->slots[slot] = peek(0);
 	    break;
 	}
 
 	case OP_JUMP_IF_FALSE: {
 	    uint16_t offset = READ_SHORT();
 	    if (isFalsey(peek(0)))
-		vm.ip += offset;
+		frame->ip += offset;
 	    break;
 	}
 
 	case OP_JUMP: {
 	    uint16_t offset = READ_SHORT();
-	    vm.ip += offset;
+	    frame->ip += offset;
 	    break;
 	}
 
 	case OP_LOOP: {
 	    uint16_t offset = READ_SHORT();
-	    vm.ip -= offset;
+	    frame->ip -= offset;
 	    break;
 	}
 
@@ -270,8 +279,10 @@ InterpretResult interpret(const char *source) {
 	return INTERPRET_COMPILE_ERROR;
 
     push(OBJ_VAL(function));
-    vm.chunk = &function->chunk;
-    vm.ip = vm.chunk->code;
+    CallFrame *frame = &vm.frames[vm.frameCount++];
+    frame->function = function;
+    frame->ip = function->chunk.code;
+    frame->slots = vm.stack;
 
     return run();
 }
